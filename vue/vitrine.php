f26451dd891b20e3b6ddaff2329a84adcd6cec29
godafflig <tombenat99@hotmail.fr>
2023-11-14 11:53:37 +0100

    ajout de la page vitrine

--- vue/vitrine.php
+++ vue/vitrine.php
@@ -6,6 +6,21 @@
     <title>Document</title>
 </head>
 <body>
+<div class="vehicle-display">
+  <div class="vehicle-item">
+    <img src="" alt="Vehicle Image" class="vehicle-image">
+    <div class="vehicle-details">
+      <h2 class="vehicle-brand">Marque du Véhicule</h2>
+      <p class="vehicle-price">Prix: €XX,XX</p>
+      <p class="vehicle-color">Couleur: Couleur du Véhicule</p>
+      <div class="vehicle-rating">
+        <span class="star-rating">★★★★★</span>
+        <span class="rating-out-of">(4.5/5)</span>
+      </div>
+    </div>
+  </div>
+  <!-- Répétez cette section pour chaque véhicule -->
+</div>
     
 </body>
 </html>
